3883d737f6d4b9048a48520bfff3353be6a31d2c
Robert Wood Jr <rdrwood@gmail.com>
2019-01-10 13:08:38 -0800

    set up C++ project for In-Class-Exercise-Decisions 1/10/19

---
+++ InClass11019/Main.cpp
@@ -0,0 +1,15 @@
+// Robert M Wood Jr.
+// CPW 218
+// 01/10/19
+// Inclass Exercise 1/10/19
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+}
